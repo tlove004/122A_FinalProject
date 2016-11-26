e7025e95c206627518a32c9fba7b48690e26d955
Tyson <tlove@ucr.edu>
2016-11-25 21:22:28 -0800

    low power (sleep), nRF two-way, and level readings through IR completed

---
+++ tlove004_FeederMaster.c
@@ -0,0 +1,216 @@
+/*  MasterServant, by Tyson Loveless
+ *    Sends control signal to a servant connected to a feeder box
+ *
+ *  Can be modified to monitor several feeder boxes by updating the
+ *    tx_address.  Multiple boxes can be controlled using the same logic
+ *
+ *  Master is actuated by either a button press on the board, through
+ *    a timer, or through the receipt of foodLevel data from a feeder
+ *    box.
+ *  
+ *  Button press and/or a timer sends a signal to all servant boxes
+ *    to run their motors for however long those are set to run.
+ *
+ *  Receipt of foodLevel data from a feeder box initiates an alert
+ *    so you can know to refill feed when it's low.
+ * 
+ *  Project completed for CS122A at UCR, Fall 2016
+ */
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
+#include "./nrf24.h"  /* credit: https://github.com/kehribar/nrf24L01_plus */
+
+uint8_t tx_address[5] = {0xE7,0xE7,0xE7,0xE7,0xE7};
+uint8_t rx_address[5] = {0xD7,0xD7,0xD7,0xD7,0xD7};
+volatile uint8_t feedIsLow;
+volatile uint8_t getFlag;
+volatile uint8_t sendFlag;
+volatile uint16_t tot_overflow;
+volatile uint8_t hours;
+//uint8_t i, led = 0;
+uint8_t *data;
+uint8_t sendData[1] = {0x77};
+
+
+/* Sends 0x77 to feeder box to initiate feed motors */
+void SendMotorSignal()
+{
+	SendRadioData(sendData);
+// 	for (i = 0; i < 10; i++) {
+// 		led = !led;
+// 		PORTA = led;
+// 		_delay_ms(500);
+// 	}
+//	_delay_ms(1500);
+// 	nrf24_csn_digitalWrite(LOW);
+// 	/* Write cmd to flush transmit FIFO */
+// 	spi_transfer(FLUSH_TX);
+// 	/* Pull up chip select */
+// 	nrf24_csn_digitalWrite(HIGH);
+	sendFlag = 0;
+}
+
+void GetFeedLevel()
+{
+	data = GetRadioData();		// get incoming data from servant
+//	PORTC = data[0];
+	if (data[0] == 0x55) {     //signal from servant?
+// 		for (i = 0; i < 6; i++) {
+// 			led = !led;
+// 			PORTA = led;
+// 			_delay_ms(500);
+// 		}
+		//check feed level
+		feedIsLow = 1;
+	}
+	else if (data[0] == 0x66) {
+		feedIsLow = 0;
+	}
+	while (nrf24_dataReady()) {
+		data = GetRadioData();
+	}
+	getFlag = 0;
+}
+
+
+
+enum MasterState {INIT, IDLE} state;
+void MasterTick()
+{
+	//Actions
+	switch (state) {
+		case INIT:
+			//setup nRF
+			InitRadio(NRF_CH, PL_SIZE);			//setup channel and payload
+			SetTransmitAddress(tx_address);		//transfer address set
+			SetRadioAddress(rx_address);		//receive address set
+		
+			//disable unneeded functions
+			power_adc_disable();
+			power_spi_disable();
+			power_twi_disable();
+			//power_timer0_disable();
+			//power_timer1_disable();
+			//power_timer2_disable();
+		
+			//get ready for sleep
+			set_sleep_mode(SLEEP_MODE_PWR_SAVE);
+			cli();									//disable interrupts
+			#if defined(BODS) && defined(BODSE)
+			sleep_bod_disable();				//brown out detection
+			#endif
+			sei();									//enable interrupts
+			state = IDLE;
+			break;
+		case IDLE:
+			//_delay_ms(10000);
+			//PORTA = PORTB = PORTC = PORTD = 0x00;
+			sleep_mode();	// sets sleep enable bit, goes to sleep
+			//wake up here,  sleep enable bit cleared at wakeup
+			cli();
+			if (sendFlag) {
+				SendMotorSignal();
+				//sei();
+			}
+			if (getFlag) {
+				GetFeedLevel();
+				if (feedIsLow) {
+					PORTA = 0x01;
+				}
+				else {
+					PORTA = 0x00;
+				}
+			}
+			sei();
+			break;
+		default:
+			state = INIT;
+			break;
+	}
+}
+
+
+int main(void)
+{
+	//Setup Ports
+	DDRA = 0x01; PORTA = 0xFE; // PA1 output, other input pull-ups
+	DDRB = 0xF8; PORTB = 0x07; // PB0:3 input pull-ups, other outputs
+	DDRC = 0x00; PORTC = 0xFF; // all input pull-ups
+	DDRD = 0x00; PORTD = 0xFF; // inputs, all pull-down (ext interrupts)
+	//PORTD |= (1<<3);		   // pull-up (btn interrupt)
+	
+	// enable external interrupt on any logic change on PD2(int0) and PD3(int1)
+	SREG |= (1<<7);
+	//INT0
+	EICRA |= (1<<ISC00);
+//	EICRA &= ~(1<<ISC01);
+	//INT1
+ 	EICRA |= (1<<ISC10);
+// 	EICRA &= ~(1<<ISC11);
+	//enable INT0 and INT1
+	EIMSK |= (1<<INT0)|(1<<INT1);  
+	
+	// set up timer interrupt
+	TCCR1B |= (1<<CS12)|(1<<CS10); // 1024 prescaler
+	TCNT1 = 0;					   // init counter
+	TIMSK1 |= (1<<TOIE1);           // enable timer1 overflow interrupt
+	tot_overflow = 0;			   // init overflow counter
+	while(1)
+	{
+		MasterTick();
+	}
+	return 0;
+}
+
+
+//interrupt on PD2 (INT0)
+ISR(INT0_vect) 
+{
+// 	for (i = 0; i < 10; i++) {
+// 		led = !led;
+// 		PORTA = led;
+// 		_delay_ms(200);
+// 	}
+//	_delay_ms(1500);
+	sendFlag = 1;
+//	_delay_ms(1000);
+}
+
+//interrupt on PD3 (INT1)
+ISR(INT1_vect)
+{
+	if (nrf24_dataReady()) {
+// 		for (i = 0; i < 10; i++) {
+// 			led = !led;
+// 			PORTA = led;
+// 			_delay_ms(2500);
+// 		}
+//		_delay_ms(1500);
+		getFlag = 1;
+		
+	}
+// 	_delay_ms(1500);
+// 	getFlag = 1;
+}
+
+ISR(TIMER1_OVF_vect)
+{
+	// keep a track of number of overflows
+	tot_overflow++;
+	
+	// check for number of overflows here itself
+	// 429 overflows == 1 hour delay (approx.)
+	if (tot_overflow >= 429)
+	{
+		if (hours++ == 12) { // every 12 hours send feed signal
+			sendFlag = 1;
+			hours = 0;
+		}
+		tot_overflow = 0;   // reset overflow counter
+	}
+}
---
+++ tlove004_FeederServant.c
@@ -0,0 +1,201 @@
+/*  FeederServant, by Tyson Loveless
+ *    Sleeps until receiving data from master to start motors
+ *     after releasing feed, it checks the level of feed remaining
+ *     in the box.  If the feed level has changed (logic low->high or
+ *     high->low), the new level is sent to the Master to alert me
+ *     to refill the box
+ *  
+ *  Project completed for CS122A at UCR, Fall 2016
+ */
+
+
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <util/delay.h>
+#include <avr/power.h>
+#include <avr/sleep.h>
+#include "./nrf24.h"  /* credit: https://github.com/kehribar/nrf24L01_plus */
+
+#define MOTORTIME 30000 //~30 seconds
+
+uint8_t tx_address[5] = {0xD7,0xD7,0xD7,0xD7,0xD7};
+uint8_t rx_address[5] = {0xE7,0xE7,0xE7,0xE7,0xE7};
+volatile uint8_t startMotor;
+volatile uint8_t getFlag = 0;
+volatile uint8_t checkFeedFlag = 0;
+volatile uint8_t feedLevel[1] = {0x00};
+volatile uint8_t prevFeedLevel[1] = {0x00};
+//uint8_t i, led = 0;
+uint8_t* data;
+uint8_t sendData[1] = {0x55};
+
+void SendFeedLevel();
+
+void RunMotors();
+
+void GetData();
+
+enum ServantState {INIT, IDLE, GO, SENDLEVEL} state;
+void ServantTick()
+{
+	//Actions
+	switch (state) {
+		case INIT:
+			//setup nRF
+			InitRadio(NRF_CH, PL_SIZE);			//setup channel and payload
+			SetTransmitAddress(tx_address);		//transfer address set
+			SetRadioAddress(rx_address);		//receive address set
+			
+			//disable unneeded functions
+			power_adc_disable();
+			power_spi_disable();
+			power_twi_disable();
+			power_timer0_disable();
+			power_timer1_disable();
+			power_timer2_disable();
+			
+			//get read for sleep
+			set_sleep_mode(SLEEP_MODE_PWR_DOWN);	//as low as 5uA power usg
+			cli();									//disable interrupts
+			#if defined(BODS) && defined(BODSE)
+				sleep_bod_disable();				//brown out detection
+			#endif
+			sei();									//enable interrupts
+			state = IDLE;
+			break;
+		case IDLE:	
+			sleep_mode();	// sets sleep enable bit, goes to sleep
+			//wake up here,  sleep enable bit cleared at wakeup
+			cli();
+			if (getFlag) {
+				GetData();
+			}
+			_delay_ms(1500);
+			if (startMotor) {
+				state = GO;   // start motors if received data to do so
+				sei();
+				break;
+			}
+			if (checkFeedFlag) {
+				state = SENDLEVEL;
+			}
+			sei();
+			break;
+		case GO:
+			cli();			  // disable interrupts
+			RunMotors();
+			sei();			  // enable interrupts
+			state = SENDLEVEL;
+			break;
+		case SENDLEVEL:
+			cli();
+			_delay_ms(1500);
+			SendFeedLevel();
+			sei();
+			state = IDLE;
+			break;
+		default:
+			state = INIT;
+			break;
+	}
+}
+
+
+int main(void)
+{
+	//Setup Ports
+	DDRA = 0xFF; PORTA = 0x00; // outputs, init to zero (Motor control)
+	DDRB = 0xFF; PORTB = 0x00; // outputs, init to zero (SPI control)
+	DDRC = 0x00; PORTC = 0x00; // input, pull-up (IR sensor[s])
+	DDRD = 0x00; PORTD = 0xFF; // inputs, pull-down (ext interrupt)
+	
+	// enable external interrupt on any logic change for PD2(int0) and PD3(int1)
+	SREG |= (1<<7);
+	//int0
+	EICRA &= ~(1<<ISC00);  
+	EICRA |= (1<<ISC01);
+	//int1
+	EICRA &= ~(1<<ISC10);
+	EICRA |= (1<<ISC11);
+	EIMSK |= (1<<INT0)|(1<<INT1);  //enable int0 and int1
+	while(1)
+	{
+		ServantTick();
+	}
+}
+
+void SendFeedLevel()
+{
+	feedLevel[0] = (PINC & 0x01);			// get feed level reading
+	// if feed level has changed, send new level
+	if (feedLevel != prevFeedLevel)
+	{
+		if (feedLevel[0]) {
+			sendData[0] = 0x55;
+		}
+		else {
+			sendData[0] = 0x66;
+		}
+//		cli();
+		SendRadioData(sendData);
+//		_delay_ms(1500);
+// 		nrf24_csn_digitalWrite(LOW);
+// 		/* Write cmd to flush transmit FIFO */
+// 		spi_transfer(FLUSH_TX);
+// 		/* Pull up chip select */
+// 		nrf24_csn_digitalWrite(HIGH);
+//		sei();
+		prevFeedLevel[0] = feedLevel[0];   //update feed memory
+	}
+	checkFeedFlag = 0;
+}
+
+void RunMotors()
+{
+	PORTA = 0x03;     // set bits 0 and 1 high on PORTA
+	_delay_ms(MOTORTIME); // run motor for MOTORTIME ms
+	PORTA = 0x00;
+	startMotor = 0;
+//	_delay_ms(1500);
+//	checkFeedFlag = 1;
+}
+
+void GetData()
+{
+// 	if (getFlag) {
+// //		for (i = 0; i < 10; i++) {
+// // 			led = !led;
+// // 			PORTA = led;
+// // 			_delay_ms(500);
+// // 		}
+	data = GetRadioData();
+//	_delay_ms(1500);
+	if (data[0] == 0x77) {
+		startMotor = 1;
+	}
+	while (nrf24_dataReady()) {
+		data = GetRadioData();
+	}
+//	}
+	getFlag = 0;
+}
+
+//interrupt service routine run when interrupted on PD2 (INT0)
+ISR(INT0_vect)
+{
+	//make sure the interrupt is due to data arriving
+	if (nrf24_dataReady()) {
+// 		for (i = 0; i < 10; i++) {
+// 			led = !led;
+// 			PORTA = led;
+// 			_delay_ms(200);
+// 		}
+//		_delay_ms(1500);
+		getFlag = 1;
+	}
+}
+
+ISR(INT1_vect) 
+{
+	checkFeedFlag = 1;
+}
